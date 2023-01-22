b439d884a00f5f26ee06845306d962832d07207d
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-01-22 00:32:58 -0300

    FirstCommit

---
+++ nmi/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Home - Empresa</title>
+
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:100,400" rel="stylesheet">
+		<script type="text/javascript" src="js/scripts.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+	</head>
+
+
+	<body>
+
+			<div id="navigation" class="navigation navigation-home">
+				<ul>
+					<li><a href="#" style="color: #7feb9b">Home</a></li>
+					<li><a href="produtos.html">Produtos</a></li>
+					<li><a href="faq.html">Perguntas Frequentes</a></li>
+					<li><a href="sobremim.html">Quem sou?</a></li>
+					<li><a href="contato.html">Contato</a></li>
+					<li id="threeline-icon" class="threeline-icon" onclick="openNav()">&#9776;</li>
+				</ul>
+			</div>
+
+
+			<div class="capa">
+
+				<div class="profile-pic">
+					<img src="img/logo.jpg" alt="Logo">
+
+					<br>
+
+					<div class="produtos-capa">
+						<a href="produtos.html">Produtos</a>
+					</div><!--produtos-capa-->
+
+				</div><!--profile-pic-->
+
+				<div class="profile-title">
+					<h1>Empresa_<span>Slogan da empresa; mensagem de bem-vindo</span></h1>
+					<h2>Cidade, UF</h2>
+					<h2>Enviamos para todo o Brasil</h2>
+					<br>
+				</div><!--profile-title-->
+
+			</div><!--capa-->
+
+	</body>
+
+</html>
